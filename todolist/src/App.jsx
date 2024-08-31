6ed3cf30d33d3b7567dd37cc63ac0787ba4aee14
Sparsh Bansal <sparshbansal787@gmail.com>
2024-08-31 19:28:11 +0530

    Initial commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+import Home from "./Home";
+import Create from "./Create";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Create />
+    </div>
+  );
+}
+
+export default App;
